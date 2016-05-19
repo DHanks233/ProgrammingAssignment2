763f48f7fcfe37c954be253c02af2759a947d4e4
Daniel Hanks <DHanks23@gmail.com>
2016-05-18 23:08:09 -0700

    Final version - cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## Pre: initialMatrix parameter is a square & invertible matrix
+## Post: None
+## Parameter: initialMatrix - A square & invertible matrix
+## Returns: A list that contains the functions which allows the
+##	    saved state of the funciton environment to accessed
+## Description: This function takes a matrix, creates functions
+##		to get, set, set the inverse and get the inverse
+##		of that matrix, which it then stores in a list that
+##		is returned. The list can be used by cacheSolve()
+##		to access the saved state of this function's environment
 
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+makeCacheMatrix <- function(initialMatrix = matrix()) {
+        inverseMatrix <- NULL
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                initialMatrix <<- y
+                inverseMatrix <<- NULL
         }
-        get <- function() x
-        setinverse <- function(solve) m <<- solve
-        getinverse <- function() m
+        get <- function() initialMatrix
+        setinverse <- function(solve) inverseMatrix <<- solve
+        getinverse <- function() inverseMatrix
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## Pre: cacheMatrix is an object created with the makeCacheMatrix
+##	function (a list of functions)
+## Post: None
+## Parameter: cacheMatrix - An object created by makeCacheMatrix
+## Returns: A matrix that is the inverse of the original matrix used
+##	    as a parameter in makeCacheMatrix
+## Description: This function uses the saved state of the makeCacheMatrix
+##		environment to read an inverse matrix. inverseMatrix is
+##		becomes part of the saved state of this environment so
+##		it can be accessed later as a quick cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(cacheMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
+        inverseMatrix <- cacheMatrix$getinverse()
+        if(!is.null(inverseMatrix)) {
                 message("getting cached data")
-                return(m)
+                return(inverseMatrix)
         }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        data <- cacheMatrix$get()
+        inverseMatrix <- solve(data, ...)
+        cacheMatrix$setinverse(inverseMatrix)
+        inverseMatrix
 }
